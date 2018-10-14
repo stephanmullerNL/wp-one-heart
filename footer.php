64066fdcef5226674dc67fbac6f6eb025ee3c6ec
Stephan Muller <stephan@frontmen.nl>
2018-10-14 15:27:00 +0200

    Make the theme look more like zenheart.nl

---
+++ footer.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the "site-content" div and all content after.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
+?>
+
+	</div><!-- .site-content -->
+
+	<footer id="colophon" role="contentinfo" class="site-info">
+        Zen Centrum Arnhem  |  C.A. Thiemestraat 26 6828 DX Arnhem
+	</footer>
+
+</div><!-- .site -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
